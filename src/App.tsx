c20a28be043ec4d4eb33288e81f5e4f4609f5ed2
aditya000099 <adi.agg099@gmail.com>
2024-02-05 13:16:08 +0530

    c1

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@
 //     </div>
 //     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color ps-4">{'</body>'}</h1>
 //     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color">{'</html>'}</h1>
-  
+
 //   </>
 //   )
 // }
@@ -87,7 +87,7 @@
 //      </div>
 //      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color ps-4">{'</body>'}</h1>
 //      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color">{'</html>'}</h1>
-  
+
 //       </>
 
 //   );
@@ -102,93 +102,144 @@
 
 
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import './App.css';
 // import ScrollMagic from 'scrollmagic';
 import gsap from 'gsap';
 import SplitTextJS from 'split-text-js';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-
+  
   useEffect(() => {
-  const skills = gsap.utils.toArray('.roles');
-  const tl = gsap.timeline({repeat: -1});
-  skills.forEach(skill =>{
-    const splitSkill = new SplitTextJS(skill);
-    tl.from(splitSkill.chars, {
-      opacity: 0,
-      y: 20,
-      rotateX: -90,
-      stagger: 0.02,
-      // duration: 2,
-      // ease: "power2.out"
-    }, "<")
-    .to(splitSkill.chars, {
-      opacity: 0,
-      y: -20,
-      rotateX: 90,
-      stagger: 0.005
-      // duration: 1,
-      // ease: "power2.out"
-    }, "<1")
-  })
-}
+    const skills = gsap.utils.toArray('.roles');
+    const tl = gsap.timeline({ repeat: -1 });
+    skills.forEach(skill => {
+      const splitSkill = new SplitTextJS(skill);
+      tl.from(splitSkill.chars, {
+        opacity: 0,
+        y: 20,
+        rotateX: -90,
+        stagger: 0.02,
+        // duration: 2,
+        // ease: "power2.out"
+      }, "<")
+        .to(splitSkill.chars, {
+          opacity: 0,
+          y: -20,
+          rotateX: 90,
+          stagger: 0.005
+          // duration: 1,
+          // ease: "power2.out"
+        }, "<1")
+    })
+  }
   )
+    // useEffect(() => {
+    //   var tl2 = gsap.timeline({
+    //     scrollTrigger: {
+    //       trigger: '.skills',
+    //       markers: true,
+    //       start: "50% 50%",
+    //       end: "100% 50%",
+    //       // scrub: 2,
+    //       pin: true,
+    //     }
+    //   })
+    //   tl2.from('.skills', {
+    //     // opacity: 0,
+    //     // top: 0,
+    //     y: 0,
+    //   })
+    // }),
+    const skills = useRef(null);
+    
+
+    useEffect(() => {
+      const skillsLength = gsap.utils.toArray('.singleSkill').length;
+      const scrollTween = gsap.to('.singleSkill', {
+        xPercent: -100 * (skillsLength - 1),
+        ease: 'none',
+        duration: 100,
+      });
+      
+        
+        ScrollTrigger.create( {
+          trigger: '.skills',
+          pin: true,
+          scrub: 1,
+          // end: "+=3000",
+          // markers: true,
+          start: "0% 0%",
+          end: "100% 100%",
+          animation: scrollTween,
+          
+        });
+      }, []);
+    
+    
+
   return (
     <>
-     <div className='html-head'>
-     <h1 className="scroll-m-40 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color">{'<html>'}</h1>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color ps-6 mt-4">{'<head>'}</h1>
-     </div>
-     <div className='html-title ps-80 mt-40 mb-40'>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color">{'<title> '}</h1>
-     <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">aditya3.eth</h1>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl mt-4">Aditya Aggarwal</h1>
-     <div className="rolesBox mt-4">
-     <div className='roles'>
-    <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700" >
-      developer
-    </h1>
-    </div>
-     <div className='roles'>
-    <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700" >
-      frontend developer
-    </h1>
-    </div>
-     <div className='roles'>
-    <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700" >
-      backend developer
-    </h1>
-    </div>
-    </div>
-      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color mt-12">{'</title>'}</h1>
-   </div>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color ps-6">{'</head>'}</h1>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color ps-4">{'<body>'}</h1>
-     <div className='html-title ps-80 mt-40 mb-40'>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color">{'<div class="skills"> '}</h1>
-     <div className="skills">
-      <div className="singleSkill">
-     <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">Web Tech</h1>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl mt-4">HTML</h1>
-     <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700">excellent</h1>
-     </div>
-      <div className="singleSkill">
-     <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">App Tech</h1>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl mt-4">Flutter</h1>
-     <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700">excellent</h1>
-     </div>
-      <div className="singleSkill">
-     <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">Blockchain</h1>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl mt-4">Solidity</h1>
-     <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700">excellent</h1>
-     </div>
-     </div>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color">{'</div>'}</h1>
-     </div>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color ps-4">{'</body>'}</h1>
-     <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color">{'</html>'}</h1>
-  
+      <div className='html-head'>
+        <h1 className="scroll-m-40 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color">{'<html>'}</h1>
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color ps-6 mt-4">{'<head>'}</h1>
+      </div>
+      <div className='html-title ps-80 mt-40 mb-40'>
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color">{'<title> '}</h1>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">aditya3.eth</h1>
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl mt-4">Aditya Aggarwal</h1>
+        <div className="rolesBox mt-4">
+          <div className='roles'>
+            <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700" >
+              developer
+            </h1>
+          </div>
+          <div className='roles'>
+            <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700" >
+              frontend developer
+            </h1>
+          </div>
+          <div className='roles'>
+            <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700" >
+              backend developer
+            </h1>
+          </div>
+        </div>
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color mt-12">{'</title>'}</h1>
+      </div>
+      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color ps-6">{'</head>'}</h1>
+      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color ps-4">{'<body>'}</h1>
+      <div className='html-title mt-40 mb-40'>
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color ps-40">{'<div class="skills"> '}</h1>
+
+
+        <div className="skills flex flex-row" ref={skills}>
+
+          <div className="singleSkill h-screen mt-4 ">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">Web Tech</h1>
+            <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl mt-4">HTML</h1>
+            <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700">excellent</h1>
+          </div>
+          <div className="singleSkill h-screen mt-4  ">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl ">App Tech</h1>
+            <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl mt-4">Flutter</h1>
+            <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700">excellent</h1>
+          </div>
+          <div className="singleSkill h-screen mt-4  ">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">Blockchain</h1>
+            <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl mt-4">Solidity</h1>
+            <h1 className="scroll-m-20 text-4xl font tracking-tight lg:text-2xl mt-8 text-gray-700">excellent</h1>
+          </div>
+        </div>
+
+
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color">{'</div>'}</h1>
+      </div>
+      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color ps-4">{'</body>'}</h1>
+      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-2xl portfolio-color">{'</html>'}</h1>
+
     </>
   );
 }
